a72b48ef66ca99b0e26f05e727eaf1495886ee1e
AlexanderJKing <94008705+AlexanderJKing@users.noreply.github.com>
2024-08-07 14:55:11 +0100

    Allowed * All Hosts

--- test_django/settings.py
+++ test_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bjowx=-xhei#tt062dcn^29v!jenh0*l)gt90j3a6v*l5@d+ae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
